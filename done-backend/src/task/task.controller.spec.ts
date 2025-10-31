b07069fff7fb3e3f89af8aec52ffc7ae3972185b
ashjay <ashwinie.ja@gmail.com>
2025-11-01 00:45:59 +0530

    test: added backend tests

--- done-backend/src/task/task.controller.spec.ts
+++ done-backend/src/task/task.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { InMemoryTaskRepository } from './in-memory-task.repository';
 
 describe('TaskController', () => {
   let controller: TaskController;
@@ -8,7 +9,7 @@ describe('TaskController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TaskController],
-      providers: [TaskService],
+      providers: [TaskService, InMemoryTaskRepository],
     }).compile();
 
     controller = module.get<TaskController>(TaskController);
--- done-backend/src/task/task.service.spec.ts
+++ done-backend/src/task/task.service.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskService } from './task.service';
+import { InMemoryTaskRepository } from './in-memory-task.repository';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 describe('TaskService', () => {
   let service: TaskService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
+      providers: [TaskService, InMemoryTaskRepository],
     }).compile();
 
     service = module.get<TaskService>(TaskService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  // Test case: check if the service is defined
+  it('should create a task', () => {
+    const dtoObject = {
+      title: 'Test Task',
+      description: 'Testing',
+    };
+
+    const task = service.createTask(dtoObject as unknown as CreateTaskDto);
+
+    expect(task).toHaveProperty('id');
+    expect(task.title).toBe(dtoObject.title);
+    expect(service.getTasks().length).toBe(3);
   });
 });
